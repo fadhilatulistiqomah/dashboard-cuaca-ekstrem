bbcdf67f35be1095a29c4e287822f83826dd23b8
fadhilatulistiqomah <fadhilahistiqomah@gmail.com>
2025-11-20 09:56:07 +0700

    Implement user authentication with hashed passwords using streamlit-authenticator in generate_keys.py; update Peta_Cuaca_Ekstrem.py for user login and data display enhancements

--- generate_keys.py
+++ generate_keys.py
@@ -1,33 +1,43 @@
 from pathlib import Path
 import pickle
-import bcrypt
+#import bcrypt
+import streamlit_authenticator as stauth    
 
-# --- Sesuaikan user / password di sini ---
-users = [
-    {"username": "sobp", "name": "Sobp", "password": "XXX"},
-    # tambah user lain kalau perlu:
-    # {"username": "user2", "name": "Nama 2", "password": "pass2"},
-]
+names =["sobp"]
+usernames = ["sobp"]
+passwords = ["XXX"]
 
-# --- Hash semua password dengan bcrypt ---
-for u in users:
-    pw_bytes = u["password"].encode("utf-8")
-    hashed = bcrypt.hashpw(pw_bytes, bcrypt.gensalt())
-    # simpan sebagai string (decode)
-    u["password"] = hashed.decode("utf-8")
+hashed_passwords = stauth.Hasher(passwords).generate()
 
-# --- Susun struktur credentials sesuai streamlit-authenticator ---
-# streamlit-authenticator mengharapkan dict: {"usernames": {username: {"name":..., "password": ...}, ...}}
-credentials = {"usernames": {}}
-for u in users:
-    credentials["usernames"][u["username"]] = {
-        "name": u["name"],
-        "password": u["password"]
-    }
-
-# --- Simpan ke file pickle ---
 file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("wb") as f:
-    pickle.dump(credentials, f)
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file) 
+# # --- Sesuaikan user / password di sini ---
+# users = [
+#     {"username": "sobp", "name": "Sobp", "password": "XXX"},
+#     # tambah user lain kalau perlu:
+#     # {"username": "user2", "name": "Nama 2", "password": "pass2"},
+# ]
 
-print(f"Saved hashed credentials to: {file_path}")
+# # --- Hash semua password dengan bcrypt ---
+# for u in users:
+#     pw_bytes = u["password"].encode("utf-8")
+#     hashed = bcrypt.hashpw(pw_bytes, bcrypt.gensalt())
+#     # simpan sebagai string (decode)
+#     u["password"] = hashed.decode("utf-8")
+
+# # --- Susun struktur credentials sesuai streamlit-authenticator ---
+# # streamlit-authenticator mengharapkan dict: {"usernames": {username: {"name":..., "password": ...}, ...}}
+# credentials = {"usernames": {}}
+# for u in users:
+#     credentials["usernames"][u["username"]] = {
+#         "name": u["name"],
+#         "password": u["password"]
+#     }
+
+# # --- Simpan ke file pickle ---
+# file_path = Path(__file__).parent / "hashed_pw.pkl"
+# with file_path.open("wb") as f:
+#     pickle.dump(credentials, f)
+
+# print(f"Saved hashed credentials to: {file_path}")
